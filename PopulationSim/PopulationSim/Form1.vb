b88e501c10fdf8ad075b8acd430611c6c8ea3d66
idsc3101 <idsc3101@gmail.com>
2013-10-09 15:14:43 -0500

    Solved

--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,13 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim year As Integer = 2012
+        Dim population As Double = 7000000000
+        Do While population > 6000000
+            population = population / 2
+            year = year - 50
+        Loop
+        MessageBox.Show("The population was less than 6 million in " & year)
 
     End Sub
 End Class
